ebe2bdd297aa06d1ecdefd43c28689070f4f4423
brijeshpatolia <bpatolialtc@gmail.com>
2024-03-11 18:02:14 +0530

    about us added

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Aboutus from './Aboutus'
 
 
 export default function Settings() {
   return (
     <div className='text-blue-800'>
-     
+        Setting
     </div>
   )
 }
